facd7702a3390ba61db6b1fdbe6b6498f6074775
Nana Kweku <kweku@Nana-Kwekus-MacBook-Pro.local>
2021-02-14 17:19:02 +0000

    system is workin

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,8 +4,8 @@ function test(){
     var message = document.getElementById("subscriptionemail").value
     url = "message" + "Laughed"
     console.log(url)
-    var number = "0545977791"
-    url = "https://apps.mnotify.net/smsapi?key=" + "XJZrndKjI8ETrsUjwXuvB6qBH&to=" + number +"&msg=" + message +"&sender_id=TestSystem"
+    var number = "0242708862,0545977791"
+    url = "https://apps.mnotify.net/smsapi?key=" + "XJZrndKjI8ETrsUjwXuvB6qBH&to=" + number +"&msg=" + message +"&sender_id=DefoProps"
     window.open(url)
 }
 

7e7c5cd3593258a60107f271c9f54ab26d93d6c5
Nana Kweku <kweku@Nana-Kwekus-MacBook-Pro.local>
2021-02-14 17:21:10 +0000

    upp

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -19,7 +19,7 @@ function wholemessage(){
   var message = "New Message from " + contactname + " " + contactemail + " " + contactsubject + " " + contactmessage
   url = "message" + "Laughed"
   console.log(url)
-  var number = "0545977791"
-  url = "https://apps.mnotify.net/smsapi?key=" + "XJZrndKjI8ETrsUjwXuvB6qBH&to=" + number +"&msg=" + message +"&sender_id=TestSystem"
+  var number = "0242708862,0545977791"
+  url = "https://apps.mnotify.net/smsapi?key=" + "XJZrndKjI8ETrsUjwXuvB6qBH&to=" + number +"&msg=" + message +"&sender_id=DefoProps"
   window.open(url)
 }
